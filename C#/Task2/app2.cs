0990d23867deb161e48dfbd730512e010a2f3830
YarNik <yarnik75@gmail.com>
2019-12-07 20:49:26 -0500

    done

---
+++ C#/Task2/app2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        class Rectangle
+        {
+            public double side1, side2;
+            public Rectangle(double s1, double s2)
+            {
+                this.side1 = s1;
+                this.side2 = s2;
+            }
+            double AreaCalculator(double side1, double side2)
+            {
+                return side1 * side2;
+            }
+
+            public double Area
+            {
+                get 
+                {
+                    return AreaCalculator(side1, side2);
+                }
+            }
+
+            double PerimeterCalculator(double side1, double side2)
+            {
+                return (side1 + side2) * 2;
+            }
+
+            public double Perimeter
+            {
+                get
+                {
+                    return PerimeterCalculator(side1, side2);
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            double num;
+            double height = 0;
+            double width = 0;
+            bool isGood;
+            do
+            {
+                Console.Write("Введите высоту прямоугольника: ");
+                string input = Console.ReadLine();
+                isGood = double.TryParse(input, out num);
+                if (isGood)
+                {
+                    height = double.Parse(input);
+                    if (height < 0) isGood = false;
+                }
+                else { isGood = false; }
+            } while (!isGood);
+
+            do
+            {
+                Console.Write("Введите ширину прямоугольника: ");
+                string input = Console.ReadLine();
+                isGood = double.TryParse(input, out num);
+                if (isGood)
+                {
+                    width = double.Parse(input);
+                    if (width < 0) isGood = false;
+                }
+                else { isGood = false; }
+            } while (!isGood);
+
+            Rectangle rec = new Rectangle(height, width);
+
+            Console.WriteLine($"Площадь этого прямоугольника {rec.Area}, а периметр {rec.Perimeter}.");
+            Console.Read();
+
+        }
+    }
+}
---
+++ C#/Task2/app4.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        class Point 
+        {
+            int x, y;
+            string name;
+            public int X 
+                { get { return x; }  }
+            public int Y
+                { get { return y; } }
+            public string Name
+            { get { return name; } }
+
+            public Point(int x, int y, string name)
+            {
+                this.x = x;
+                this.y = y;
+                this.name = name;
+            }
+        }
+
+        class Figure
+        {
+            Point a, b, c, d, e;
+            public string figName;
+            public Figure(Point a, Point b, Point c) 
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+            public Figure(Point a, Point b, Point c, Point d)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+                this.d = d;
+            }
+            public Figure(Point a, Point b, Point c, Point d, Point e)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+                this.d = d;
+                this.e = e;
+            }
+
+            double LengthSide(Point A, Point B) 
+            {
+                return Math.Sqrt(Math.Pow((A.X - B.X), 2) + Math.Pow((A.Y - B.Y), 2));
+            }
+
+            public double PerimeterCalculator()
+            {
+                double result = 0;
+                if (d == null & e == null)
+                    { result = LengthSide(a, b) + LengthSide(b, c) + LengthSide(c, a);
+                    figName = a.Name + b.Name + c.Name;
+                    }
+                if (e == null & d != null)
+                    { result = LengthSide(a, b) + LengthSide(b, c) + LengthSide(c, d) + LengthSide(d, a);
+                    figName = a.Name + b.Name + c.Name + d.Name;
+                }
+                if (e != null & d != null)
+                    { result = LengthSide(a, b) + LengthSide(b, c) + LengthSide(c, d) + LengthSide(d, e) + LengthSide(e, a);
+                    figName = a.Name + b.Name + c.Name + d.Name + e.Name;
+                }
+                
+                return result;
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Point A = new Point(2, 2, "A");
+            Point B = new Point(10, 6, "B");
+            Point C = new Point(10, 2, "C");
+            Point D = new Point(9, 0, "D");
+            Point E = new Point(2, 0, "E");
+
+            Figure abc = new Figure(A, B, C);
+            double abcPerimeter = abc.PerimeterCalculator();
+            Figure abcd = new Figure(A, B, C, D);
+            double abcdPerimeter = abcd.PerimeterCalculator();
+            Figure abcde = new Figure(A, B, C, D, E);
+            double abcdePerimeter = abcde.PerimeterCalculator();
+
+            Console.WriteLine($"Периметр {abc.figName} составляет {abcPerimeter}");
+            Console.WriteLine($"Периметр {abcd.figName} составляет {abcdPerimeter}");
+            Console.WriteLine($"Периметр {abcde.figName} составляет {abcdePerimeter}");
+            
+            Console.Read();
+
+        }
+    }
+}
