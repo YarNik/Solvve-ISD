ae9de18131c16af69e1747d7eee9383adea907e3
YarNik <yarnik75@gmail.com>
2019-12-08 20:17:38 -0500

    Create app5.cs
    done

---
+++ C#/Task2/app5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        class User 
+        {
+            public string login, name, lastName;
+            public int age;
+            public readonly string date;
+            public User(string login, string name, string lastName, int age, string date) 
+            {
+                this.login = login;
+                this.name = name;
+                this.lastName = lastName;
+                this.age = age;
+                this.date = date;
+            }
+            public void Show() 
+            {
+                Console.WriteLine($"Login: {login}, Name: {name}, Last Name: {lastName}, Age: {age}, Date: {date}");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            User Vasya = new User("Vas", "Vasiliy", "Ivanov", 45, "December 12 2019");
+            Vasya.login = "admin";
+            Vasya.Show();
+
+            Console.Read();
+           
+        }
+    }
+}
