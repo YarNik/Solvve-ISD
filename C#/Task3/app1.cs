fbd6ddba1d28e851f04070e17eca663aed9679c5
YarNik <yarnik75@gmail.com>
2019-12-15 17:06:17 -0500

    fixed and added

--- C#/Task3/app1.cs
+++ C#/Task3/app1.cs
@@ -15,8 +15,7 @@ namespace ConsoleApp1
         }
 
         class PrinterYellow : Printer
-        {
-           
+        {           
             public override void Print(string value) 
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
@@ -27,8 +26,7 @@ namespace ConsoleApp1
         }
 
         class PrinterGreen : Printer
-        {
-           
+        {           
             public override void Print(string value)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -46,10 +44,15 @@ namespace ConsoleApp1
             PrinterGreen pGreen = new PrinterGreen();
             pGreen.Print("Print PrintGreen");
 
-            Printer pBase2 = pGreen;                             // почему то не работает приведение типов
-            pBase2.Print("Print Printer");
+            Printer pBase2 = pGreen;
+            pBase2.Print("Print Printer2");
 
-            PrinterYellow pYellow2 = (PrinterYellow)pBase;        // не работает приведение типов (выбрасывает Exeption)
+            Printer pBase3 = new PrinterYellow();
+            PrinterYellow pYellow2 = (PrinterYellow)pBase3;
+            pYellow2.Print("Print PrinterYellow2");
+
+            Printer pBase4 = new PrinterGreen();
+            ((PrinterGreen)pBase4).Print("Print PrintGreen2");
 
         }
     }
---
+++ C#/Task3/app4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        class DoсumentWorker 
+        {
+            public void OpenDocument() { Console.WriteLine("Документ открыт."); }
+            public virtual void EditDocument() { Console.WriteLine("Редактирование документа доступно в версии Про."); }
+            public virtual void SaveDocument() { Console.WriteLine("Сохранение документа доступно в версии Про."); }
+            public void Show()
+            {
+                OpenDocument();
+                EditDocument();
+                SaveDocument();                
+            }
+        }
+
+        class ProDocumentWorker : DoсumentWorker 
+        {
+            public override void EditDocument() { Console.WriteLine("Документ отредактирован."); }
+            public override void SaveDocument() { Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт."); }
+        }
+
+        class ExpertDocumentWorker : ProDocumentWorker
+        {           
+            public override void SaveDocument() { Console.WriteLine("Документ сохранен в новом формате."); }
+        }
+        static void Main(string[] args)
+        {      
+            string key;
+            do
+            {
+                Console.Write("Введите ключ доступа pro(1111) или exp(2222): ");
+                key = Console.ReadLine();
+                if (key == "1111") break;
+                if (key == "2222") break;
+                if (key == "") break;
+            } while (true);
+            if (key == "") 
+            {
+                DoсumentWorker dw = new DoсumentWorker();
+                dw.Show();
+            }
+            if (key == "1111") 
+            { 
+                DoсumentWorker pdw = new ProDocumentWorker();
+                pdw.Show();
+            }
+            if (key == "2222")
+            {
+                DoсumentWorker edw = new ExpertDocumentWorker();
+                edw.Show();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
