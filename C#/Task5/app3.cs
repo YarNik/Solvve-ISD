9f6603ec72ad92886ecc8e79c141ea45a9cddc28
YarNik <yarnik75@gmail.com>
2020-01-06 21:26:30 +0200

    Create app3.cs

---
+++ C#/Task5/app3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class MyClass
+    {
+        public string change;
+    }
+
+    struct MyStruct
+    {
+        public string change;
+    }
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            myClass.change = "не изменено";
+            myStruct.change = "не изменено";
+            ClassTaker(myClass);
+            StruktTaker(myStruct);
+            MyClass myClass2 = new MyClass();
+            Console.WriteLine($"myClass.change: {myClass.change}");     // "изменено"
+            Console.WriteLine($"myStruct.change: {myStruct.change}");   // "не изменено"      
+            Console.ReadLine();
+        }
+    }
+}
+
+        /*  Метод StruktTaker не отработал (по моему мнению) потому что при передаче параметра 
+         *  myStruct в метод StruktTaker он передается как локальная копия переменной myStruct
+         *  (т.к. MyStruct это структура).
+         *  Метод StruktTaker отработал потому что передача параметра myClass прошла как передача
+         *  ссылки на  myClass (т.к. MyClass это класс).   */
