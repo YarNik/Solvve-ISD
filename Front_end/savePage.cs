10db28b0a11e4cded5b6b3f74b5af697ed5dd32c
YarNik <yarnik75@gmail.com>
2020-03-06 19:16:01 +0200

    Create savePage.cs

---
+++ Front_end/savePage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace SavePage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the needed site (selin.in.ua)");
+            string siteName = Console.ReadLine();
+            Console.WriteLine("Enter the file name (savedPage)");
+            string fileName = Console.ReadLine();
+            //string fileName = "savedPage";
+            //string siteName = "selin.in.ua";
+            DirectoryInfo dirInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);            
+            var directory = dirInfo.Parent.Parent.Parent;       // file will be saved in root directory of project
+            string writePath = String.Format(@"{0}\{1}.txt", directory, fileName);
+            string site = String.Format("http://www.{0}", siteName);
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(site);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                using (StreamReader stream = new StreamReader(
+                     response.GetResponseStream(), Encoding.UTF8))
+                {
+                    string Page = stream.ReadToEnd();
+                    System.IO.File.WriteAllText(writePath, Page);                    
+                    Console.WriteLine(Page);                    
+                    Console.ReadLine();
+                }
+                response.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception {0}", e);
+                Console.ReadLine();
+            }
+        }
+    }
+}
