685634751a478fd78015e9b09c3b3e7ff82c02bc
YarNik <yarnik75@gmail.com>
2020-03-08 20:56:58 +0200

    Update savePageV2.cs

--- Front_end/savePageV2.cs
+++ Front_end/savePageV2.cs
@@ -2,27 +2,31 @@
 using System.IO;
 using System.Net.Sockets;
 
-namespace SavePageVer_2
+namespace SavePageV3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the needed site (selin.in.ua)");
-            string serverAddr = Console.ReadLine();
-            Console.WriteLine("Enter the file name (output.html)");
-            string fileName = Console.ReadLine();
-            //string fileName = "output.html";
-            //string serverAddr = "selin.in.ua";
+            //Console.WriteLine("Enter the needed site (http://selin.in.ua/solvve/html.html)");
+            //string fullUrl = Console.ReadLine();
+            //Console.WriteLine("Enter the file name (output.html)");
+            //string fileName = Console.ReadLine();
+            string fileName = "output.html";            
+            string fullUrl = "http://selin.in.ua/solvve/html.html";
             DirectoryInfo dirInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
             var directory = dirInfo.Parent.Parent.Parent;     // file will be saved in root directory of project
             string writePath = String.Format(@"{0}\{1}", directory, fileName);
             
-            var message = "GET /index.html HTTP/1.0\n\n";
             try
             {
-                var port = 80;                
-                var client = new TcpClient(serverAddr, port);
+                string fullUrlNotHttp = fullUrl.IndexOf("http://") == -1 ? fullUrl : fullUrl.Substring(7);
+                string host = fullUrlNotHttp.IndexOf("/") == -1 ? fullUrlNotHttp : fullUrlNotHttp.Substring(0, fullUrlNotHttp.IndexOf("/"));
+                string pageAddr = fullUrlNotHttp.IndexOf("/") == -1 ? "" : fullUrlNotHttp.Substring(fullUrlNotHttp.IndexOf("/") + 1);                
+                string message = String.Format("GET /{0} HTTP/1.0\nHost: {1}\n\n", pageAddr, host);
+                var port = 80;
+
+                var client = new TcpClient(host, port);
                 var data = System.Text.Encoding.UTF8.GetBytes(message);
                 NetworkStream stream = client.GetStream();
                 stream.Write(data, 0, data.Length);
@@ -31,10 +35,9 @@ namespace SavePageVer_2
                 int bytesRead = stream.Read(responseData, 0, responseData.Length);
                 var responseMessage = System.Text.Encoding.UTF8.GetString(responseData, 0, bytesRead);
                 Console.WriteLine("Received {0}", responseMessage);
-
-                string bodyResponse = responseMessage.Substring(responseMessage.IndexOf("\n\n"));
+                string bodyResponse = responseMessage.Substring(responseMessage.IndexOf("\r\n\r\n"));                
                 System.IO.File.WriteAllText(writePath, bodyResponse);
-                //Console.WriteLine(bodyResponse);
+                //Console.WriteLine(bodyResponse);                
                 Console.ReadLine();
                 stream.Close();
                 client.Close();
@@ -47,4 +50,3 @@ namespace SavePageVer_2
         }
     }
 }
-    
