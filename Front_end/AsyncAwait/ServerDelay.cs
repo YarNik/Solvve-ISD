2730bf783c1b8dde6f79b614d88638f5ee29ae4e
YarNik <yarnik75@gmail.com>
2020-04-02 11:01:42 +0300

    Done

---
+++ Front_end/AsyncAwait/ServerDelay.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerDelay
+{
+    public class ClientСonnection
+    {
+        public TcpClient client;
+        public ClientСonnection(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }        
+        public void ServerResponse()
+        {            
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] bytes = new byte[64];                
+                    int bytesReadCount = stream.Read(bytes, 0, bytes.Length);
+                    string request = System.Text.Encoding.UTF8.GetString(bytes, 0, bytesReadCount);
+                    Console.WriteLine($"Received: {request}.");
+                    int number;
+                    int data;
+                    bool isInt = int.TryParse(request, out number);
+                    if (isInt) data = int.Parse(request) * int.Parse(request);
+                        else data = -1;
+                    byte[] response = System.Text.Encoding.UTF8.GetBytes(data.ToString());
+                    Thread.Sleep(500);
+                    stream.Write(response, 0, response.Length);
+                    Console.WriteLine($"Sent: {data}.");
+                    stream.Close();
+                    client.Close();               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        const int port = 13000;
+        static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Waiting for connections...");
+                while (true)
+                {
+                    TcpClient tcpClient = listener.AcceptTcpClient();
+                    ClientСonnection newClient = new ClientСonnection(tcpClient);
+                    Thread clientThread = new Thread(new ThreadStart(newClient.ServerResponse));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Front_end/AsyncAwait/SynchronyClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace SynchronyClient
+{
+    class Program
+    {
+        const int port = 13000;
+        const string address = "127.0.0.1";
+        static TcpClient client = null;
+        static void WebRequest(int i)
+        {
+            try
+            {
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+                byte[] request = System.Text.Encoding.UTF8.GetBytes(i.ToString());
+                stream.Write(request, 0, request.Length);                
+                var responseData = new byte[256];
+                int bytesRead = stream.Read(responseData, 0, responseData.Length);
+                var responseMessage = System.Text.Encoding.UTF8.GetString(responseData, 0, bytesRead);               
+                Console.WriteLine($"Sent {i}, received {responseMessage}");
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int i = 1; i <= 10; i++)
+            {
+                WebRequest(i);
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}.{1:00}",
+                ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.ReadLine();
+        }
+    }
+}
